d14702d3b4d783e359bf219d3dee9540d52aeca0
MatteoFilippini <matteofilippini8@gmail.com>
2022-03-14 16:25:01 +0100

    linked contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,12 +5,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Contacts</title>
 </head>
 
 <body>
-    <h1>Questa è la homepage</h1>
-    <a href="{{ url('/contact') }}">Contatti</a>
+    Qui ci sono i contatti
 </body>
 
 </html>
